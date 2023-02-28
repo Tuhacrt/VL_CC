426ccaa17a792412a898270dcca1217c53329a1b
Tuhacrt <a067800497@gmail.com>
2023-02-28 14:32:48 +0800

    deploy

--- assets/IndexView-b70cd593.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index-f10d7621.js";const o={};function r(t,a){return c(),n("div",null,"Home page")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ assets/NotFound-9e34f9ec.js
@@ -0,0 +1,1 @@
+import{_ as o,o as e,c}from"./index-d4ef98da.js";const n={};function t(r,s){return e(),c("div",null,"404 Not Found")}const a=o(n,[["render",t]]);export{a as default};
